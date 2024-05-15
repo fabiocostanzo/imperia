6b0dbe3305d8e532344c6912a83e9cc64dfa492b
Fabio Costanzo <fabiocostanzo1996@gmail.com>
2024-05-15 22:16:47 +0200

    feat(configuration): add authentication with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
