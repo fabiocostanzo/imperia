2775f93dae6da0b375c7868e1cbd924e9eb662f9
Fabio Costanzo <fabiocostanzo1996@gmail.com>
2024-05-22 12:10:14 +0200

    feat(auth): add authentication with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'firebase_options.dart';
+import 'package:imperia/auth.dart';
+import './pages/home.dart';
+import './pages/login.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -21,76 +20,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Imperia',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const InitialNavigation(),
     );
   }
 }
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({ super.key });
-
-  void onPressed() {
-    print('test');
-  }
+class InitialNavigation extends StatefulWidget {
+  const InitialNavigation({super.key});
 
   @override
+  State<InitialNavigation> createState() => _InitialNavigationState();
+}
+
+class _InitialNavigationState extends State<InitialNavigation> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          alignment: Alignment.center, 
-          padding: EdgeInsets.all(24),
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Imperia', style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.w600,
-            )),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'username',
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'password',
-              ),
-              obscureText: true,
-            ),
-             ElevatedButton(
-              onPressed: onPressed,
-              child: Text('Next'),
-            ),
-          ],
-        ),
-          
-        ),
-      )
-    );
-    
-        
-    
-  }}
-
-
+    return StreamBuilder(
+        stream: Auth().authStateChanges,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return const LoginPage();
+          }
+        });
+  }
+}
